98bbea84db485b9f70f4bbbf3eb4127ab7f17e02
aliifz <aliifz.ws@alice-smith.edu.my>
2026-01-26 13:35:56 +0800

    test command

--- routes/console.php
+++ routes/console.php
@@ -5,4 +5,5 @@ use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
+    Log::info('Inspiring quote: ' . Inspiring::quote());
 })->purpose('Display an inspiring quote');
